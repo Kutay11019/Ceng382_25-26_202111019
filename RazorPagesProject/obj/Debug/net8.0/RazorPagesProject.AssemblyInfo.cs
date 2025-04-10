63b838a001308488d742e94eb16090a7eb92c1b0
Kutay11019 <118713769+Kutay11019@users.noreply.github.com>
2025-04-10 14:56:03 +0300

    bakk

--- RazorPagesProject/obj/Debug/net8.0/RazorPagesProject.AssemblyInfo.cs
+++ RazorPagesProject/obj/Debug/net8.0/RazorPagesProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+278620e17f33bee12af5fd744d614388be3476b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c8c5b83de83eee575124b8d61e782b3c7ce17d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
