ec983b12d3fbd12a274482f79f39fb2243eff21d
Kutay11019 <118713769+Kutay11019@users.noreply.github.com>
2025-04-30 14:48:42 +0300

    Week8 added

--- RazorPagesProject/Pages/Logout.cshtml.cs
+++ RazorPagesProject/Pages/Logout.cshtml.cs
@@ -1,12 +1,19 @@
-public class LogoutModel : PageModel
-{
-    public IActionResult OnGet()
-    {
-        HttpContext.Session.Clear();
-        Response.Cookies.Delete("username");
-        Response.Cookies.Delete("token");
-        Response.Cookies.Delete("session_id");
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
 
-        return RedirectToPage("Login");
+namespace RazorPagesProject.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            HttpContext.Session.Clear();
+            Response.Cookies.Delete("username");
+            Response.Cookies.Delete("token");
+            Response.Cookies.Delete("session_id");
+
+            return RedirectToPage("/Login");
+        }
     }
 }
